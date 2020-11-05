d3d0bf4f21484c9d09ccf76340ec83d2b18f829d
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2020-11-05 09:53:17 +0000

    Added GUI folders, and view models and views as per user story 'Separate views for each service'

---
+++ SelfAssessment_Files/SelfAssessmentService_WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfAssessmentService_WPF.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+    }
+}
