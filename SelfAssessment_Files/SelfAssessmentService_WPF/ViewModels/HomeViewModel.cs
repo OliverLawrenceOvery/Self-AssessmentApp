0bc1681b48149c088b258c9755c37a1d9924373f
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2020-11-06 19:01:04 +0000

    Improved the GUI for the login and customised the home view as per user story 'Home View contents'

--- SelfAssessment_Files/SelfAssessmentService_WPF/ViewModels/HomeViewModel.cs
+++ SelfAssessment_Files/SelfAssessmentService_WPF/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SelfAssessmentService_WPF.State.Authenticator;
+﻿using SelfAssessmentService_Domain.Models;
+using SelfAssessmentService_WPF.State.Authenticator;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +9,13 @@ namespace SelfAssessmentService_WPF.ViewModels
     public class HomeViewModel : BaseViewModel
     {
         private IAuthenticator _authenticator;
+        public Account CurrentAccount { get; set; }
 
         public HomeViewModel(IAuthenticator authenticator)
         {
             _authenticator = authenticator;
+            CurrentAccount = _authenticator.CurrentAccount;
+            
         }
     }
 }
